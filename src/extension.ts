cfe87e7c13463dd9fb2cf4100af0877fb111d242
Chentai Kao <ru6xul6@gmail.com>
2022-07-06 16:37:21 -0700

    try out text input box

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, ExtensionContext, extensions, window } from 'vscode';
 import { GitExtension } from './git';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
@@ -15,22 +15,53 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    'who-knows-about.helloWorld',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from who-knows-about!');
-    }
-  );
-  test();
 
-  context.subscriptions.push(disposable);
+  registerHelloWorld(context);
+  registerWhoKnowsAbout(context);
+
+  test();
+}
+
+function registerHelloWorld(context: ExtensionContext): void {
+  context.subscriptions.push(
+    commands.registerCommand('who-knows-about.helloWorld', () => {
+      window.showInformationMessage('Hello world!');
+    })
+  );
+}
+
+function registerWhoKnowsAbout(context: ExtensionContext): void {
+  context.subscriptions.push(
+    commands.registerCommand('who-knows-about.whoknowsabout', () => {
+      const quickPick = window.createQuickPick();
+      quickPick.items = [{ label: 'showInputBox' }];
+      quickPick.onDidChangeSelection(async (items) => {
+        if (items[0]) {
+          await showInputBox();
+        }
+      });
+      quickPick.onDidHide(() => quickPick.dispose());
+      quickPick.show();
+    })
+  );
+}
+
+async function showInputBox(): Promise<void> {
+  const result = await window.showInputBox({
+    value: 'abcdef',
+    valueSelection: [2, 4],
+    placeHolder: 'For example: fedcba. But not: 123',
+    validateInput: (text) => {
+      window.showInformationMessage(`Validating: ${text}`);
+      return text === '123' ? 'Not 123!' : null;
+    },
+  });
+  window.showInformationMessage(`Got: ${result}`);
 }
 
 async function test(): Promise<void> {
   const gitExtension =
-    vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+    extensions.getExtension<GitExtension>('vscode.git')?.exports;
   if (gitExtension) {
     const api = gitExtension.getAPI(1);
     const repo = api.repositories[0];
