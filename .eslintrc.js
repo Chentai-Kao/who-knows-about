3d3c2c1a891b8ace137cfd18810c30ec5e918e6f
Chentai Kao <ru6xul6@gmail.com>
2022-07-06 15:53:16 -0700

    try out git extension API

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['**/dist/**/*'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['eslint-plugin-prettier', '@typescript-eslint', 'unused-imports'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+      },
+    ],
+  },
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,69 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { GitExtension } from './git';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "who-knows-about" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "who-knows-about" is now active!'
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('who-knows-about.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from who-knows-about!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    'who-knows-about.helloWorld',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage('Hello World from who-knows-about!');
+    }
+  );
+  test();
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+}
+
+async function test(): Promise<void> {
+  const gitExtension =
+    vscode.extensions.getExtension<GitExtension>('vscode.git')?.exports;
+  if (gitExtension) {
+    const api = gitExtension.getAPI(1);
+    const repo = api.repositories[0];
+    const head = repo.state.HEAD;
+
+    // Get the branch and commit
+    if (head) {
+      const { commit, name: branch } = head;
+
+      // Get head of any other branch
+      const mainBranch = 'master';
+      const branchDetails = await repo.getBranch(mainBranch);
+
+      if (branch) {
+        // Get last merge commit
+        const lastMergeCommit = await repo.getMergeBase(branch, mainBranch);
+
+        const status = await repo.status();
+
+        console.log({
+          branch,
+          commit,
+          lastMergeCommit,
+          needsSync: lastMergeCommit !== commit,
+          branchDetails,
+          status,
+        });
+      }
+    }
+  }
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // nothing
+}
